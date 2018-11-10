1bc3afac02b73a6624c8746b689b00d2bf9f1725
SvetG <38772377+SvetG@users.noreply.github.com>
2018-11-10 13:36:34 -0500

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React from 'react';
+import { Route } from 'react-router-dom';
+
+import SearchPage from './SearchPage';
+import MainPage from './MainPage';
+
+import * as BooksAPI from './BooksAPI';
+import './App.css';
+
+class BooksApp extends React.Component {
+  state = {
+    books: []
+  }
+
+  componentDidMount() {
+    BooksAPI.getAll().then((books) => {
+      this.setState({ books: books })
+    })
+  }
+
+  changeShelf = (book,shelf) => {
+    BooksAPI.update(book, shelf);
+    BooksAPI.getAll().then((books) => {
+        this.setState({ books: books })
+    })
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <Route exact path="/" render={() => (
+          <MainPage
+            books={this.state.books}
+            changeShelf={this.changeShelf}
+          />
+        )} />
+        <Route exact path="/search" render={() => (
+          <SearchPage
+            changeShelf={this.changeShelf}
+            books={this.state.books}
+          />
+        )} />
+      </div>
+    )
+  }
+}
+
+export default BooksApp
